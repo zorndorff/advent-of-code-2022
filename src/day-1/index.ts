9da4f0a8da56cada013f140e1e601707a1b9e249
Zac Orndorff <zac@orndorff.dev>
2022-12-01 12:12:48 -0600

    updates

---
+++ src/day-1/index.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from 'node:fs';
+import {join} from 'node:path';
+
+import { solve } from './day-1';
+
+function main() {
+  const input = readFileSync(join(__dirname, './input.txt'))
+    .toString()
+    .split('\n');
+  
+  const output = solve(input);
+  console.log(output);
+
+  const total = output.reduce((acc, elf) => {
+    acc += elf.total;
+    return acc;
+  }, 0);
+
+  console.log(`Rich Elf total : ${total}`);
+}
+
+main();
